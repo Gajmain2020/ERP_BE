432b060e01f148c0df9d3da5980ae3cad019ecf1
Gajendra Sahu <gajmain2020@gmail.com>
2025-04-16 16:26:40 +0530

    fix: response for get assignmnet fixed

--- src/models/assignment.models.ts
+++ src/models/assignment.models.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const AssignmentSchema = new mongoose.Schema(
   {
     assignmentNumber: {
-      type: Number,
+      type: String,
       required: true,
     },
     assignmentName: {
